b232537a26fac3007cff3044a3a64513dcf3be55
Rafael Lopes <rafael.lopes@univision.net.br>
2021-08-16 12:10:26 -0300

    Configuração inicial do Eureka

--- src/main/java/br/com/integra/eureka/Application.java
+++ src/main/java/br/com/integra/eureka/Application.java
@@ -1,8 +1,12 @@
-package br.com.integra.zuul;
+package br.com.integra.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
